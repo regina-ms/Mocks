336a537da647a42fd38f9f67f74e219463ec9a8a
Regina <rmyachina@mail.ru>
2023-06-18 16:55:54 +0300

    init

---
+++ src/__tests__/getLevel-test.js
@@ -0,0 +1,26 @@
+import getLevel from '../getLevel';
+import fetchData from '../http';
+
+jest.mock('../http.js');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('should return "Информация об уровне временно недоступна"', () => {
+  fetchData.mockReturnValue(new Error('Mock this!'));
+  expect(getLevel(45)).toBe('Информация об уровне временно недоступна');
+});
+
+test('should return "Ваш текущий уровень: 99"', () => {
+  const response = {
+    status: 'ok',
+    level: 99,
+  };
+  fetchData.mockReturnValue(response);
+  expect(getLevel(46)).toBe('Ваш текущий уровень: 99');
+});
+
+/* test('mock was called', () => {
+  expect(() => { fetchData('https://server/user/45'); }).toThrow();
+}); */
---
+++ src/http.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+export default function fetchData(url) {
+  throw new Error('Mock this!');
+}
